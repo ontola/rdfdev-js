f8677d3efaf85e5bacedf81bf4cb265543416503
Thom <thom@argu.co>
2022-09-12 16:06:42 +0200

    fix: don't interface unused method

--- packages/collections/src/types.ts
+++ packages/collections/src/types.ts
@@ -1,7 +1,6 @@
 import { Node, Quad, Term } from "@ontologies/core";
 
 export interface Store {
-  addQuads(data: Quad[]): Quad[];
   getResourceProperty<T extends Term = Term>(
     subject: Node,
     property: Node | Node[],
